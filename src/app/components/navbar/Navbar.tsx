fcbf38382cd6b9f69108e24405c864733d9e526c
Theecypher <obioramma12@gmail.com>
2026-07-03 11:21:41 +0100

    added the logo and icons

--- src/app/components/navbar/Navbar.tsx
+++ src/app/components/navbar/Navbar.tsx
@@ -1,12 +1,27 @@
-import Image from "next/image";
+'use client'
+
+import Image, { ImageLoaderProps } from "next/image";
 import { Menu, Search, ShoppingBag, User, X } from "lucide-react";
 import Link from "next/link";
 
 const Navbar = () => {
+//   const imageLoader = ({ src, width, quality }: ImageLoaderProps) => {
+//     return `/assets/novalogo.png/${src}?w=${width}&q=${quality || 75}`;
+//   };
+
+//   const imageStyle = {
+//     borderRadius: "50%",
+//     border: "1px solid #fff",
+//   };
+
   return (
     <div className="flex justify-between bg-white items-center px-5">
       <div className="flex gap-5 items-center">
-        <Image alt="logo" src="/assets/novalogo.png" width={130} height={170} />
+        <Image alt="logo" src="/assets/novalogo.png" 
+        sizes="(max-width: 768px) 100px, (max-width: 1200px) 1000px, 500px"
+        width={130} 
+        height={170} 
+        />
 
         <div className="hidden lg:flex">
           {/* Categories of clothes/shoes */}
