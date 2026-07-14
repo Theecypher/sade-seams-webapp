6feb52c00eb45621987f5c95dac10db5f0d98915
Theecypher <obioramma12@gmail.com>
2026-07-14 16:41:59 +0100

    completed navbar search ui

--- src/app/components/ui/Search.tsx
+++ src/app/components/ui/Search.tsx
@@ -1,11 +1,14 @@
+import { Search } from "lucide-react";
+
 const NavSearch = () => {
-    return ( 
-        <div>
-            <form>
-                <input type="text" placeholder="" />
-            </form>
-        </div>
-     );
-}
- 
+  return (
+    <div>
+      <div className="flex gap-3 border rounded-[30px] px-5 w-100 py-3">
+        <Search color="black" />
+        <input type="text" placeholder="Search within women's clothes" className="text-black outline-none border-none w-full text-sm" />
+      </div>
+    </div>
+  );
+};
+
 export default NavSearch;
